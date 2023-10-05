b98a449bbf5c526bb902d3db69e504f172fd6d8c
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2023-10-05 11:23:45 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
 function divideAndConquerSum(a) {
-    return a;
+    if(a.length == 1) return a[0];
+    if(a.length == 2) return a[0] + a[1];
+    if(a.length == 0) {
+        return 0;}
+    else{
+        return SumCalc(a, a.length - 1, 0); }
+}
+
+function SumCalc(a, hi, lo) {
+    var mid = lo + (hi - lo) / 3;
+    var mid2 = mid + (hi - lo) / 3;
+
+    return SumCalc(a, mid - 1, lo) + SumCalc(a, mid2 - 1, mid + 1) + SumCalc(a, hi, mid2 + 1);
 }
